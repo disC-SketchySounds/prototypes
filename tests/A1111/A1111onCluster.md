0c50283c12d2a333bbf3144fe047ac043c6198f3
Philipp Kremling <philipp.kremling.pk@gmail.com>
2023-12-05 01:47:24 +0100

    Adds A1111 installation instructions

---
+++ tests/A1111/A1111onCluster.md
@@ -0,0 +1,25 @@
+# AUTOMATIC1111 - stable-diffusion-webui auf HPC Cluster ausführen
+1. `git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git`
+2. Datei "webui-user.sh" anpassen: Torch-Command einkommentieren
+3. Datei "webui-user.sh" anpassen: `export COMMANDLINE_ARGS="--ckpt-dir '/nfs/scratch/students/<USER>/content'"`
+4. `module load python/anaconda3 cuda/cuda-12.3`
+5. `conda create --name py39`
+6. `conda activate py39`
+7. sbatchJobSD.sh hochladen
+8. `sbatch sbatchJobSD.sh` 
+   1. Hinweis: Der Job läuft vor allem beim ersten Mal starten deutlich länger. Der Log kann sich live mit `tail -f <FILE>` angesehen werden.
+9. Lokal: `ssh -N -L 127.0.0.1:7860:127.0.0.1:7860 <USER>@<NODE>.informatik.fh-nuernberg.de -i ~/.ssh/<KEY>`
+
+Anschließend kann A1111 lokal über 127.0.0.1:7860 aufgerufen werden.
+
+## Falls Modelle noch nicht vorhanden:
+1. `mkdir /nfs/scratch/students/<USER>/content`
+2. Gute Qualität: `wget https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors?download=true`
+3. Sehr gute Qualität (in Verbindung mit Base): `wget https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors?download=true`
+4. Extrem schnell: `wget https://huggingface.co/stabilityai/sdxl-turbo/resolve/main/sd_xl_turbo_1.0_fp16.safetensors?download=true`
+5. Umbenennen der Dateien (ohne "?download=true")
+
+## Konfiguration der Modelle (abweichend vom Standard):
+1. SDXL Base: Bildgröße = 1024x1024
+2. SDXL Base + Refiner: Bildgröße = 1024x1024, bei Refiner das Modell einstellen, Sampling-Schritte = 30, Switch = 0,6
+3. SDXL Turbo: Sampling Methode = Euler a, Sampling-Schritte = 1, CFG-Scale = 1 
---
+++ tests/A1111/sbatchJobSD.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#SBATCH --job-name=sd-tuning   # Kurzname des Jobs
+#SBATCH --nodes=1                 # Anzahl benötigter Knoten
+#SBATCH --ntasks=1                # Gesamtzahl der Tasks über alle Knoten hinweg
+#SBATCH --partition=p0            # Verwendete Partition (z.B. p0, p1, p2 oder all)
+#SBATCH --time=08:00:00           # Gesamtlimit für Laufzeit des Jobs (Format: HH:MM:SS)
+#SBATCH --cpus-per-task=8         # Rechenkerne pro Task
+#SBATCH --mem=24G                 # Gesamter Hauptspeicher pro Knoten
+#SBATCH --gres=gpu:1              # Gesamtzahl GPUs pro Knoten
+#SBATCH --qos=basic               # Quality-of-Service
+#SBATCH --mail-type=ALL           # Art des Mailversands (gültige Werte z.B. ALL, BEGIN, END, FAIL oder REQUEUE)
+#SBATCH --mail-user=kremlingph95027@th-nuernberg.de # Emailadresse für Statusmails
+
+echo "=================================================================="
+echo "Starting Batch Job at $(date)"
+echo "Job submitted to partition ${SLURM_JOB_PARTITION} on ${SLURM_CLUSTER_NAME}"
+echo "Job name: ${SLURM_JOB_NAME}, Job ID: ${SLURM_JOB_ID}"
+echo "Requested ${SLURM_CPUS_ON_NODE} CPUs on compute node $(hostname)"
+echo "Working directory: $(pwd)"
+echo "=================================================================="
+
+###################### Optional for Pythonnutzer*innen #######################
+# Die folgenden Umgebungsvariablen stellen sicher, dass
+# Modellgewichte von Huggingface und PIP Packages nicht unter 
+# /home/$USER/.cache landen. 
+CACHE_DIR=/nfs/scratch/students/$USER/.cache
+export PIP_CACHE_DIR=$CACHE_DIR
+export TRANSFORMERS_CACHE=$CACHE_DIR
+export HF_HOME=$CACHE_DIR
+mkdir -p "$CACHE_DIR"
+########################################################
+
+module purge
+module load python/anaconda3 cuda/cuda-12.3
+eval "$(conda shell.bash hook)"
+conda activate py39
+
+############### Starte eigenen Job hier ################
+srun ./stable-diffusion-webui/webui.sh
