c39350e5025cbe394fb4f99da89194e56f3708c4
Philipp Kremling <philipp.kremling.pk@gmail.com>
2024-01-02 11:25:41 +0100

    Adds Compel for SDXL Turbo

--- api/Service.py
+++ api/Service.py
@@ -145,11 +145,16 @@ def call_sdxl_turbo(transaction_id):
     pipe.to("cuda")
     pipe.enable_model_cpu_offload()
     pipe.enable_xformers_memory_efficient_attention()
+    compel = Compel(tokenizer=[pipe.tokenizer, pipe.tokenizer_2],
+                    text_encoder=[pipe.text_encoder, pipe.text_encoder_2],
+                    returned_embeddings_type=ReturnedEmbeddingsType.PENULTIMATE_HIDDEN_STATES_NON_NORMALIZED,
+                    requires_pooled=[False, True])
 
     prompt = f"""
             Create a musical score with the following attributes: {analysis}     
             """
-    image = pipe(prompt=prompt).images[0]
+    conditioning, pooled = compel(prompt)
+    image = pipe(prompt_embeds=conditioning, pooled_prompt_embeds=pooled, num_inference_steps=30).images[0]
     img_byte_arr = io.BytesIO()
     image.save(img_byte_arr, format='JPEG')
     img_byte_arr.seek(0)
